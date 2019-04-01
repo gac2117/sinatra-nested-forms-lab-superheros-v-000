6318d2c0c1eb93182b121b3fffca7d1fc588d721
Grace Choi <oiograceoio@gmail.com>
2019-04-01 20:39:14 +0000

    Automatically backed up by Learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,15 +1,15 @@
 class SuperHero
-  attr_accessor :name, :power, :bio 
+  attr_accessor :name, :power, :bio
   @@all = []
 
   def initialize(params)
     @name = params[:name]
     @power = params[:power]
     @bio = params[:bio]
-    @@all << self 
+    @@all << self
   end
 
-  def self.all 
-    @@all 
+  def self.all
+    @@all
   end
 end
