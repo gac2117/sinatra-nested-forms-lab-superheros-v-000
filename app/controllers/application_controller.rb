a0e08e25b65b80a740c6a4c5bb051f59a834e800
Grace Choi <oiograceoio@gmail.com>
2019-04-01 20:22:34 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  set :views, Proc.new { File.join(root, "../views/") }
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  get '/' do
+    erb :super_hero
+  end
 
-
+  post '/teams' do
+    
+  end
 end
